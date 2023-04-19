be48a254f723f7b0643b93540796f7283a4aaffc
Funda Yıldırımkaraman <108402804+fundayldrmkaraman@users.noreply.github.com>
2023-04-19 22:01:25 +0300

    Add files via upload

---
+++ Algorithm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir kelime ve çıkarmak istenilen karakter indeksi giriniz (Arada virgül olmalıdır):");
+            string k = Console.ReadLine();
+
+            string[] input = k.Split(",");
+            string word = input[0];
+            int number = Convert.ToInt32(input[1]);
+            string newWord = word.Remove(number, 1);
+
+            Console.WriteLine(newWord);
+        }
+    }
+}
