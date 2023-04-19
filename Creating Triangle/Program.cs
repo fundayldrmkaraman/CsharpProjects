45d68efedbecb17b2835c9f480667f5ce30e350f
Funda Yıldırımkaraman <108402804+fundayldrmkaraman@users.noreply.github.com>
2023-04-19 19:57:07 +0300

    Add files via upload

---
+++ Creating Triangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Creating_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayi giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=sayi;i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write(".");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
