6cab0bd1d9d41bec9d9b5d515d2183577ac40d9b
Funda Yıldırımkaraman <108402804+fundayldrmkaraman@users.noreply.github.com>
2023-04-19 22:12:00 +0300

    Add files via upload

---
+++ Reverse String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Reverse_String
+{
+    internal class Program
+    {
+        public static string ReverseString(string str)
+        {
+            char[] chars = str.ToCharArray();
+            string reverse=String.Empty;
+            for(int i=chars.Length-1; i>-1; i--)
+            {
+                reverse += chars[i];
+            }
+            return reverse;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseString("I Love You"));
+        }
+    }
+}
