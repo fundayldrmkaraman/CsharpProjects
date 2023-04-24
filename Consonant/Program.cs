5ffa996e31e57c2ea78c69beb897d569ad082544
Funda Yıldırımkaraman <108402804+fundayldrmkaraman@users.noreply.github.com>
2023-04-24 20:31:54 +0300

    Add files via upload

---
+++ Consonant/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Consonant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metin giriniz : ");
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(' ');
+            string consonant = "bcdfghjklmnpqrstvwxyz";
+
+            foreach (string word in words)
+            {
+                bool hasConsecutiveConsonants = false;
+                for (int i = 0; i < word.Length - 1; i++)
+                {
+                    if (consonant.Contains(word[i]) && consonant.Contains(word[i + 1]))
+                    {
+                        hasConsecutiveConsonants = true;
+                        break;
+                    }
+                }
+                Console.Write(hasConsecutiveConsonants + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
