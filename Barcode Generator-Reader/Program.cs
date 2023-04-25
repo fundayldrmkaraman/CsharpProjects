4efff9f14dbd1493f3c981ae594d3147a8a9afbb
Funda Yıldırımkaraman <108402804+fundayldrmkaraman@users.noreply.github.com>
2023-04-26 00:20:13 +0300

    Add files via upload

---
+++ Barcode Generator-Reader/Program.cs
@@ -0,0 +1,23 @@
+﻿using BarcodeLib;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace CSharpBarcodeGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Barcode barcodLib = new Barcode();
+
+            int imageWidth = 290;  
+            int imageHeight = 120; 
+            Color foreColor = Color.Black; 
+            Color backColor = Color.White; 
+
+            string NumericString = "038000350216";
+            Image barcodeImage = barcodLib.Encode(TYPE.UPCA, NumericString, foreColor, backColor, imageWidth, imageHeight);
+            barcodeImage.Save(@"C:\Users\Lenovo\Desktop\Barcode2.png", ImageFormat.Png);
+        }
+    }
+}
